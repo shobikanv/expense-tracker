9984dd522f2413f7214deac1a35ed3a786dce562
shobika24 <shobika@sedintechnologies.com>
2023-04-25 12:20:41 +0530

    removed unwanted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ForgetPassword from "./components/Unwanted/ForgetPassword";
-import Analytics from "./components/Unwanted/Analytics";
 import Layout from "./components/Layout";
-import NoPage from "./components/NoPage";
-import Registration from "./components/Unwanted/Registration";
-import SignIn from "./components/Unwanted/SignIn";
-import ExpenseForm from "./components/Unwanted/ExpenseForm";
-import IncomeForm from "./components/Unwanted/IncomeForm";
 import IndexPage from "./components/IndexPage";
 import "./components/App.css";
 import RecentTrans from "./components/Transactions/RecentTrans";
 import TransactionForm from "./components/Transactions/TransactionForm";
-import EIForm from "./components/Unwanted/EIForm";
 import AccountForm from "./components/Accounts/AccountSetup";
 import TransactionFilterForm from "./components/Transactions/TransactionFilterForm";
 import TransactionAnalytics from "./components/Reports";
 import MyComponenet from "./components/Unwanted/FilteringDate";
-import CurrencyExchange from "./components/Settings/CurrencyExchange";
 import Settings from "./components/Settings/Settings";
 
 function App() {
@@ -26,13 +17,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} />
-          <Route path="registration" element={<Registration />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path="forgotpassword" element={<ForgetPassword />} />
-          <Route path="expense-form" element={<ExpenseForm />} />
-          <Route path="income-form" element={<IncomeForm />} />
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="trans" element={<EIForm />} />
 
           <Route path="account" element={<AccountForm />} />
           <Route path="transactions" element={<TransactionForm />} />
